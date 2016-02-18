dff9a404914daab1c36a7a29c8f3ce899dd811e3
Nimpen Jimmy Nordström <j@asbra.nu>
2016-02-18 13:39:33 +0100

    First upload

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # GenRandID
 PHP Class for generating unique non chronologic ids from a number list
+
+# Example
+
+<?php
+require_once('class.asbraUniqueID.php');
+$ab=new asbraUniqueID();
+
+$i=1;
+while ($i <= 500) {
+	echo "$i ::"; 
+	$enc=$ab->encode($i);
+	echo "$enc ::";
+	echo $ab->decode($enc)." <br />";
+	$i++;
+}
+?>
---
+++ class.GenRandID.php
@@ -0,0 +1,38 @@
+<?php
+/*
+** ASBRA AB, www.asbra.nu <j@asbra.nu>
+** GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
+**
+** EXAMPLE:
+** $ab=new GenRandID();
+** $enc=$ab->encode('15');
+** $dec=$ab->decode('8dw');
+*/
+
+class asbraUniqueID {
+
+	// All the vowels, some consonants and some numbers have been removed
+	public $TR_ENC = '56789dfghjklmnpqrstvwz';
+	
+	function __construct() {
+		$this->BASE=strlen($this->TR_ENC);
+		$this->TR_DEC=substr('0123456789abcdefghijklmnopqrstuvwxyz',0,$this->BASE);
+		}
+
+	public function encode($userID) {
+		$rand=rand(10,99);
+		$newID=$userID.$rand;
+		$base=base_convert($newID, 10, $this->BASE);	
+		$transl=strtr($base, $this->TR_DEC, $this->TR_ENC);
+		return($transl);
+		}
+
+	public function decode($userID) {
+		$transl=strtr($userID, $this->TR_ENC, $this->TR_DEC);
+		$base=base_convert($transl, $this->BASE, 10);	
+		$userID=substr($base, 0, -2);
+		return($userID);
+		}
+}
+?>
+
